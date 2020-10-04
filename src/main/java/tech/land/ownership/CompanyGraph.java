272629869584e3c1cce4e091fe08a017aeba24e2
Vijay Patel <veejspeej@gmail.com>
2020-10-04 12:42:27 +0100

    Adds test for building company graph

--- src/main/java/tech/land/ownership/CompanyGraph.java
+++ src/main/java/tech/land/ownership/CompanyGraph.java
@@ -1,4 +1,25 @@
 package tech.land.ownership;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+
 public class CompanyGraph {
+    private final Map<String, Set<String>> graph;
+    private Map<String, String> graphReverse;
+
+    public CompanyGraph(Map<String, String> graphReverse) {
+        this.graphReverse = graphReverse;
+        this.graph = this.graphReverse.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() != null)
+                .collect(groupingBy(Map.Entry::getValue, mapping(Map.Entry::getKey, Collectors.toSet())));
+    }
+
+    public Map<String, Set<String>> getGraph() {
+        return this.graph;
+    }
 }
--- src/test/java/tech/land/ownership/CompanyGraphTest.java
+++ src/test/java/tech/land/ownership/CompanyGraphTest.java
@@ -1,12 +1,42 @@
 package tech.land.ownership;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompanyGraphTest {
 
     @Test
-    public void testGraph() {
-        Assert.fail("Test not implemented yet");
+    public void testBuildsGraph() {
+        Map<String, String> reverseGraph = new HashMap<>();
+        reverseGraph.put("A", null);
+        reverseGraph.put("B", "A");
+        reverseGraph.put("C", "A");
+        reverseGraph.put("D", "A");
+        reverseGraph.put("E", "C");
+        reverseGraph.put("F", "E");
+        reverseGraph.put("G", "E");
+        reverseGraph.put("H", "D");
+
+        CompanyGraph companyGraph = new CompanyGraph(reverseGraph);
+
+        Map<String, Set<String>> forwardGraph = companyGraph.getGraph();
+
+        assertThat(forwardGraph).hasSize(4);
+        assertThat(forwardGraph).containsEntry("A", setOf("B", "C", "D"));
+        assertThat(forwardGraph).containsEntry("C", setOf("E"));
+        assertThat(forwardGraph).containsEntry("E", setOf("F", "G"));
+        assertThat(forwardGraph).containsEntry("D", setOf("H"));
     }
+
+    public static HashSet<String> setOf(String... companyIds) {
+        return newHashSet(companyIds);
+    }
+
 }
