10faf483aee3a09cc9bbecdee2380a72c573cae5
Vijay Patel <veejspeej@gmail.com>
2020-10-04 12:35:30 +0100

    Adds base project structure

---
+++ src/main/java/tech/land/ownership/CompanyGraph.java
@@ -0,0 +1,4 @@
+package tech.land.ownership;
+
+public class CompanyGraph {
+}
---
+++ src/test/java/tech/land/ownership/CompanyGraphTest.java
@@ -0,0 +1,12 @@
+package tech.land.ownership;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CompanyGraphTest {
+
+    @Test
+    public void testGraph() {
+        Assert.fail("Test not implemented yet");
+    }
+}
